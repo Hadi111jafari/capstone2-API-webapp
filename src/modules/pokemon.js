942b0e38d27bce7dde947ea9e7a6f5eff4be3a98
memelopez <meme30lopez@gmail.com>
2022-04-21 14:01:22 -0600

    Modify pokemon class to store comments, comments counter, and likes counter

--- src/modules/pokemon.js
+++ src/modules/pokemon.js
@@ -1,11 +1,15 @@
 // /src/modules/pokemon.js - Pokemon class
 
 export default class Pokemon {
-  constructor(id, name, height, weight, image) {
+  constructor(id, name, height, weight, image, backImg) {
     this.id = id;
     this.name = name.charAt(0).toUpperCase() + name.slice(1);
     this.height = height;
     this.weight = weight;
     this.image = image;
+    this.backImage = backImg;
+    this.likesCounter = 0;
+    this.commentsCounter = 0;
+    this.comments = [];
   }
 }
