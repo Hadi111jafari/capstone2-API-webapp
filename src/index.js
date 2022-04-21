9d3aeda6861c06a5bd67a824212c741578cb56e6
memelopez <meme30lopez@gmail.com>
2022-04-20 23:13:10 -0600

    Ad event listener for clicks in the pokemon list

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
-import './style.css';
+// /src/index.js - entry point
 
+import './style.css';
 import Pokemon from './modules/pokemon.js';
 import UI from './modules/UI.js';
 import PokemonAPI from './modules/pokemonAPI.js';
+
 // variables
 const pokeList = [];
+
+const modal = document.getElementById('myModal');
+const closeBtn = document.querySelector('.close');
+
 // methods
 const transform2poke = (elem) => {
   const neo = new Pokemon(elem.id, elem.name, elem.height, elem.weight, elem.sprites.front_default);
   pokeList.push(neo);
 };
+
 const initPoke = () => {
   PokemonAPI.getInitialList().then((res) => {
     const tempList = res.results;
@@ -22,5 +29,32 @@ const initPoke = () => {
   })
     .catch((error) => error);
 };
+
 // Event: on content load
 document.addEventListener('DOMContentLoaded', initPoke());
+
+// Event: click anything on the pokemon list
+document.querySelector('#pokemons').addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const classesTarget = e.target.className;
+  const classesArr = classesTarget.split(' ');
+  if (classesArr.indexOf('commentBtn') !== -1) {
+    const idstr = e.target.id;
+    const idArr = idstr.split('-');
+    const id = idArr[1];
+    pokeList.forEach((poke) => {
+      // console.log('-->index.js - poke.id: ', poke.id, ' and id:', id);
+      if (poke.id === parseInt(id, 10)) {
+        // console.log('-->index.js - poke: ', poke);
+        UI.addInfoModal(poke);
+        modal.style.display = 'block';
+      }
+    });
+  }
+});
+
+closeBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  modal.style.display = 'none';
+});
